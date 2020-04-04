13f47e393e52982bea674c304986c0371f0387b3
NeoAtlantis <contact@chaobai.li>
2020-04-04 05:49:40 +0200

    core renamed

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ const datastore = {
     '8fba4998-8f12-4e0f-b561-2c5d5e71e497': 'Kx+Hi6qkBCFpnIXwoVxOjG81oH0oFZaoKCktOFL9Esy1S6Q6Ycq9mrizYOcjNy8eDaMPqs8lyqQdtRKHvAidCTs95mLprV7kskuhfPIQoFUjXKXbyRPJjI10tvhp+qeVkQ/LX9jA4hh5UZREWrhN7bMo1ao8ZeuDKcA7E6zyw7sO/8708oCFlZhfs8Jj77Ch6hIRIsVIGTjS7HhtZfjFDA=='
 };
 
-const VolatilePassword = require("./core")({
+const VolatilePasswords = require("./volatile-passwords")({
     read: async function(key){
         return datastore[key];
     },
@@ -29,16 +29,16 @@ const salt2 = "salt2";
 
 
 console.log("password1");
-console.log(await VolatilePassword(UUID, password1, salt1));
+console.log(await VolatilePasswords(UUID, password1, salt1));
 
 console.log("password1");
-console.log(await VolatilePassword(UUID, password1, salt1));
+console.log(await VolatilePasswords(UUID, password1, salt1));
 
 console.log("password2");
-console.log(await VolatilePassword(UUID, password2, salt1));
+console.log(await VolatilePasswords(UUID, password2, salt1));
 
 console.log("password1");
-console.log(await VolatilePassword(UUID, password1, salt1));
+console.log(await VolatilePasswords(UUID, password1, salt1));
 
 
 
